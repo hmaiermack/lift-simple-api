5dddc3d82ab8b5793628f2712ddbdcb563ebb1a2
Mack Maier <hmaiermack@gmail.com>
2020-07-11 20:06:33 -0700

    update config vars

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     CLIENT_ORIGIN: 'https://lift-simple-app.now.sh/',
-    DATABASE_URL: process.env.DB_URL || 'postgresql://postgres:candid55@localhost/lift-simple-api',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres:candid55@localhost/lift-simple-api',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres:candid55@localhost/lift-simple-test'
   }
